3be46b2f79c3d6467638cf7f1c415f2147304acc
AlexandrDaultukaev <alexandrdaultukaev@gmail.com>
2022-08-31 19:46:38 +0700

    Fix pipe

--- task7_bash/my_bash/main.c
+++ task7_bash/my_bash/main.c
@@ -3,78 +3,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <ctype.h>
 
-/* Функция без использования pipe */
-// void run_process(char** cmd1) {
-//     int status;
-//     pid_t pid = fork();
-//     if (pid == -1) {
-//         fprintf(stderr, "Can't clone process: %s\n", strerror(errno));
-//     }
+struct fd_pipe {
+    int fd[2];
+} typedef fd_pipe;
 
-//     if (!pid) {
-//         int res = execvp(cmd1[0], cmd1);
-//         if (res == -1)
-//             fprintf(stderr, "Execvp failed: %s\n", strerror(errno));
-//         exit(EXIT_SUCCESS);
-//     } else {
-//         wait(&status);
-//     }
-// }
+// #define PRINT_CMDS
 
-// /* cmds представляет собой массив команд, разделенные каналами:
-//  * Пример: $ ls | grep ake | grep 135
-//  *
-//  * cmds = {    0        1
-//  *      0|    "ls",
-//  *      1|    "grep", "ake",
-//  *      2|    "grep", "135",
-//  *        }
+void run_pipe(char*** cmds, int num_pipes) {
+    fd_pipe fdesc[num_pipes+1];
+    for(int i = 0; i <= num_pipes; i++) {
+        if (pipe(fdesc[i].fd) == -1) {
+            fprintf(stderr, "pipe error\n");
+        }
+    }
+    int status = 0;
+    pid_t pid;
+    int i;
+    for (i = 0; i <= num_pipes; i++) {
+        pid = fork();
+        if (!pid) {
+            if(i != num_pipes) {
+                dup2(fdesc[i].fd[0], 0);   //pipe_in <-- stdin
+                dup2(fdesc[i+1].fd[1], 1); //stdout  --> pipe_out
+            } else if(i != 0) {
+                dup2(fdesc[i].fd[0], 0);
+            }
+            close(fdesc[i].fd[0]);
+            close(fdesc[i].fd[1]);
 
-// /* Функция с использованием pipe */
-// void run_pipe(char* cmds[20][20], int num_pipes) {
-//     int fd[2];
-//     if (pipe(fd) == -1) {
-//         fprintf(stderr, "pipe error\n");
-//     }
-//     int status = 0;
-//     pid_t wpid;
-//     int row = 0;
-//     pid_t* pid = malloc(sizeof(pid_t) * num_pipes);
-//     for (int i = 0; i <= num_pipes; i++) {
-//         pid[i] = fork();
-//         if (!pid[i]) {
-//             dup2(fd[0], 0);
-//             if (i != num_pipes) {
-//                 dup2(fd[1], 1);
-//             }
-//             close(fd[0]);
-//             close(fd[1]);
-
-//             execvp(cmds[row][0], cmds[row]);
-//             perror("execvp");
-//             exit(EXIT_FAILURE);
-//         }
-//         row++;
-//     }
-//     close(fd[0]);
-//     close(fd[1]);
-
-//     do {
-//         for (int i = 0; i < num_pipes; i++) {
-//             waitpid(pid[i], &status, WUNTRACED);
-//         }
-//     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-//     free(pid);
-// }
+            execvp(cmds[i][0], cmds[i]);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+        close(fdesc[i].fd[0]);
+        close(fdesc[i].fd[1]);
+        do {
+            waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    
+}
 
 int main(int argc, char* argv[]) {
-    /* my_args - одномерный массив команд = {"ls", "grep", "..."} */
+    /* cmds представляет собой массив команд, разделенные каналами:
+ * Пример: $ ls | grep ake | grep 135
+ *
+ * cmds = {    0        1
+ *      0|    "ls",
+ *      1|    "grep", "ake",
+ *      2|    "grep", "135",
+ *        }
+ */
     char*** cmds;
-
     int row;
     int ch_num;
     int command_num;
@@ -101,7 +86,8 @@ int main(int argc, char* argv[]) {
         for(int i = 0; i < 128; i++) {
             if(entire_command[i] == ' ') {
                 cmds[row][command_num][ch_num] = '\0';
-                if(entire_command[i-1] != '|' && isalpha(entire_command[i+1])) {
+                // Если пробел сделан после | то ничего не делаем, иначе засчитываем как новую команду
+                if(entire_command[i-1] != '|' && (isalpha(entire_command[i+1]) || entire_command[i+1] == '-')) {
                     cmds[row][++command_num] = (char*)malloc(sizeof(char) * 20);
                 }
                 ch_num = 0;
@@ -119,15 +105,23 @@ int main(int argc, char* argv[]) {
                 cmds[row][command_num][ch_num++] = entire_command[i];
             }
         }
+
+        #ifdef PRINT_CMDS
         printf("row: %d\n", row);
         for(int i = 0; i <= row; i++) {
             printf("[ ");
             for(int j = 0; cmds[i][j] != NULL; j++) {
 
                 printf("%s, ", cmds[i][j]);
+                if(cmds[i][j+1] == NULL) {
+                    printf("NULL ");
+                }
             }
             printf(" ]\n");
         }
+        #endif
+        run_pipe(cmds, row);
+        
         for(int i = 0; i <= row; i++) {
             for(int j = 0; cmds[i][j] != NULL; j++) {
                 free(cmds[i][j]);
@@ -136,6 +130,7 @@ int main(int argc, char* argv[]) {
             free(cmds[i]);
             
         }
+        
         free(cmds);
     }
     return 0;
